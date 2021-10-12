17cd1ff9e3b969ddde6f7c058ae8958c3422a995
safwan <safwanbutto@outlook.com>
2021-10-12 12:59:05 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+float var(float* x, int size){
+    var = 0;
+    for( n = 0; n < numPoints; n++ )
+    {
+        var += (Array[n] - mean) * (Array[n] - mean);
+    }
+    var /= numPoints;
+    sd = sqrt(var);
+}
